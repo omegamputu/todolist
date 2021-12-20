1198680ffc4f3409ae4d022be91e0b2d984058d2
Omega Mputu <kutekemeni93@gmail.com>
2021-12-20 14:11:17 +0100

    Création d'un champs d'ajout de nouvelle tâche

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -2,14 +2,44 @@
     <section class="todoapp">
         <header class="header">
             <h1>Todos</h1>
+            <input type="text" class="new-todo" placeholder="Ajouter une tâche" v-model="newTodo" @keyup.enter="addTodo">
         </header>
+        <div class="main">
+            <ul class="todo-list">
+                <li class="todo" v-for="todo in todos" :key="todo.id">
+                    <div class="view">
+                        <label>{{ todo.name }}</label>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </section>
 </template>
 
 <script>
 
 export default {
-
+    data () {
+        return {
+            todos: [
+                {
+                    id: 0,
+                    name: 'Tâche de test',
+                    completed: false
+                }
+            ],
+            newTodo: '',
+        }
+    },
+    methods: {
+        addTodo () {
+            this.todos.push({
+                name: this.newTodo,
+                completed: false
+            })
+            this.newTodo = ''
+        }
+    }
 }
 </script>
 
